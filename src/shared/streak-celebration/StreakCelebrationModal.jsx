60bdaf591ccc96d47a7ce05e59a712409a80568f
Michael Terry <mterry@edx.org>
2021-09-28 16:44:05 -0400

    fix: don't show 3-day streak discount if it won't provide a discount
    Also, this will pull the actual discount percent from ecommerce,
    instead of hardcoding it.
    
    AA-1012

--- src/shared/streak-celebration/StreakCelebrationModal.jsx
+++ src/shared/streak-celebration/StreakCelebrationModal.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { camelCaseObject, getConfig } from '@edx/frontend-platform';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import {
   FormattedMessage, injectIntl, intlShape,
 } from '@edx/frontend-platform/i18n';
@@ -41,6 +43,13 @@ function getRandomFactoid(intl, streakLength) {
   return factoids[Math.floor(Math.random() * (factoids.length))];
 }
 
+async function calculateVoucherDiscount(voucher, sku, username) {
+  const urlBase = `${getConfig().ECOMMERCE_BASE_URL}/api/v2/baskets/calculate`;
+  const url = `${urlBase}/?code=${voucher}&sku=${sku}&username=${username}`;
+  return getAuthenticatedHttpClient().get(url)
+    .then(res => camelCaseObject(res));
+}
+
 function StreakModal({
   courseId, metadataModel, streakLengthToCelebrate, intl, isStreakCelebrationOpen,
   closeStreakCelebration, StreakDiscountCouponEnabled, verifiedMode, ...rest
@@ -48,10 +57,12 @@ function StreakModal({
   if (!isStreakCelebrationOpen) {
     return null;
   }
-  const { org, celebrations } = useModel(metadataModel, courseId);
+  const { org, celebrations, username } = useModel(metadataModel, courseId);
   const factoid = getRandomFactoid(intl, streakLengthToCelebrate);
   // eslint-disable-next-line no-unused-vars
   const [randomFactoid, setRandomFactoid] = useState(factoid); // Don't change factoid on re-render
+
+  const [discountPercent, setDiscountPercent] = useState(0);
 
   const layout = layoutGenerator({
     mobile: 0,
@@ -68,6 +79,28 @@ function StreakModal({
     }
   }, [isStreakCelebrationOpen, org, courseId]);
 
+  // Ask ecommerce to calculate discount savings
+  useEffect(() => {
+    if (verifiedMode && getConfig().ECOMMERCE_BASE_URL) {
+      // Open edX Folks! If you create a voucher with this code, we'll notice and show the discount in the streak modal
+      calculateVoucherDiscount('ZGY11119949', verifiedMode.sku, username)
+        .then(
+          (result) => {
+            const { totalInclTax, totalInclTaxExclDiscounts } = result.data;
+            if (totalInclTaxExclDiscounts && totalInclTax !== totalInclTaxExclDiscounts) {
+              // Just store the percent (rather than using these values directly), because ecommerce doesn't give us
+              // the currency symbol to use. So we'll keep using the values in verifiedMode, just multiplied.
+              setDiscountPercent(1 - totalInclTax / totalInclTaxExclDiscounts);
+              // FIXME: send segment alert here too
+            }
+          },
+          () => {
+            // ignore any errors - we just won't show the discount to the user then
+          },
+        );
+    }
+  }, [username, verifiedMode]);
+
   function CloseText() {
     return (
       <span>
@@ -82,6 +115,8 @@ function StreakModal({
   let offer;
 
   if (verifiedMode) {
+    // FIXME: is it OK to pass this code in, if it doesn't exist? (thinking of Open edX)
+    // If not, set it on the offer's upgradeUrl below
     upgradeUrl = `${verifiedMode.upgradeUrl}&code=ZGY11119949`;
     mode = {
       currencySymbol: verifiedMode.currencySymbol,
@@ -89,14 +124,18 @@ function StreakModal({
       upgradeUrl,
     };
 
-    offer = {
-      discountedPrice: `${verifiedMode.currencySymbol}${(mode.price * 0.85).toFixed(2).toString()}`,
-      originalPrice: `${verifiedMode.currencySymbol}${mode.price.toString()}`,
-      upgradeUrl: mode.upgradeUrl,
-    };
+    if (discountPercent) {
+      const discountMultipler = 1 - discountPercent;
+      offer = {
+        discountedPrice: `${verifiedMode.currencySymbol}${(mode.price * discountMultipler).toFixed(2).toString()}`,
+        originalPrice: `${verifiedMode.currencySymbol}${mode.price.toString()}`,
+        upgradeUrl: mode.upgradeUrl,
+      };
+    }
   }
 
   const title = `${streakLengthToCelebrate} ${intl.formatMessage(messages.streakHeader)}`;
+  const showOffer = offer && StreakDiscountCouponEnabled;
 
   return (
     <ModalDialog
@@ -125,7 +164,7 @@ function StreakModal({
             <img src={StreakDesktopImage} alt="" className="img-fluid" />
           </OnDesktop>
         </p>
-        { !StreakDiscountCouponEnabled && (
+        { !showOffer && (
           <div className="d-flex py-3 bg-light-300">
             <Icon className="col-small ml-3" src={Lightbulb} />
             <div className="col-11 factoid-wrapper">
@@ -133,13 +172,13 @@ function StreakModal({
             </div>
           </div>
         )}
-        { StreakDiscountCouponEnabled && (
+        { showOffer && (
           <Alert variant="success" className="px-0">
             <div className="d-flex">
               <Icon className="col-small ml-3 text-success-500" src={MoneyFilled} />
               <div className="col-11 factoid-wrapper">
                 <b>{intl.formatMessage(messages.congratulations)}</b>
-                &nbsp;{intl.formatMessage(messages.streakDiscountMessage)}&nbsp;
+                &nbsp;{intl.formatMessage(messages.streakDiscountMessage, { percent: (discountPercent * 100).toFixed(0) })}&nbsp;
                 <FormattedMessage
                   id="learning.streakCelebration.streakCelebrationCouponEndDateMessage"
                   defaultMessage="Ends {date}."
@@ -153,7 +192,7 @@ function StreakModal({
         )}
       </ModalDialog.Body>
       <ModalDialog.Footer className="modal-footer d-block">
-        { StreakDiscountCouponEnabled && (
+        { showOffer && (
           <>
             <OnMobile>
               <UpgradeNowButton
@@ -180,7 +219,7 @@ function StreakModal({
             </OnDesktop>
           </>
         )}
-        { !StreakDiscountCouponEnabled && (
+        { !showOffer && (
           <ModalDialog.CloseButton className="px-5" variant="primary"><CloseText /></ModalDialog.CloseButton>
         )}
       </ModalDialog.Footer>
